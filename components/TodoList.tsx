0df132d8ef15d3a930c77f99e5faefb0f3f3a89e
haicao <hai.cao@ffw.com>
2024-06-18 10:26:18 +0700

    feat: Create todo list

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Todo } from '../interfaces/Todo';
+import TodoItem from './TodoItem';
 import TodoForm from './TodoForm';
 
 const TodoList: React.FC = () => {
@@ -18,6 +19,21 @@ const TodoList: React.FC = () => {
     saveTodoList(newList);
   };
 
+  const deleteTodo = (id: number) => {
+    const newList = todos.filter(todo => todo.id !== id);
+    saveTodoList(newList);
+  };
+
+  const editTodo = (id: number, newTitle: string, newDesc: string) => {
+    const newList = todos.map(todo => (todo.id === id ? { ...todo, title: newTitle, desc: newDesc } : todo));
+    saveTodoList(newList);
+  };
+
+  const toggleTodo = (id: number) => {
+    const newList = todos.map(todo => (todo.id === id ? { ...todo, completed: !todo.completed } : todo));
+    saveTodoList(newList);
+  };
+
   const saveTodoList = (list: Todo[]) => {
     setTodos(list);
     localStorage.setItem('todos', JSON.stringify(list));
@@ -26,6 +42,20 @@ const TodoList: React.FC = () => {
   return (
     <div className="todo-list">
       <TodoForm onAddTodo={addTodo} />
+      <div className="p-4 bg-[#ECEDF6] rounded-lg mt-4">
+      {todos.map(todo => (
+        <TodoItem
+          id={todo.id}
+          key={todo.id}
+          title={todo.title}
+          desc={todo.desc}
+          completed={todo.completed}
+          onDelete={deleteTodo}
+          onEdit={editTodo}
+          onToggle={toggleTodo}
+        />
+      ))}
+      </div>
     </div>
   );
 };
